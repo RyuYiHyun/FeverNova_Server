6ec2d3e24a06d73355a1773ee7e1c431a9d49c95
이준영 <jyl1035@gmail.com>
2022-10-23 20:51:29 +0900

    이준영_서버작업
    1234

--- GameManager.h
+++ GameManager.h
@@ -24,15 +24,15 @@ private:
 public:
 	enum class E_PROTOCOL
 	{
-		// ÃÊ±â ÇÊ¿ä °ª
-		CRYPTOKEY,		// ÃÊ±â ¾Ïº¹È£È­Å° Àü¼Û ½ÅÈ£
-		IDCREATE,		// ¾ÆÀÌµð »ý¼º
+		// ï¿½Ê±ï¿½ ï¿½Ê¿ï¿½ ï¿½ï¿½
+		CRYPTOKEY,		// ï¿½Ê±ï¿½ ï¿½Ïºï¿½È£È­Å° ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È£
+		IDCREATE,		// ï¿½ï¿½ï¿½Ìµï¿½ ï¿½ï¿½ï¿½ï¿½
 
-		PLAYTYPE,		// ¸ÖÆ¼ or ½Ì±Û ¼±ÅÃ
-		WAIT,			// ¸ÖÆ¼ ´ë±â
-		SINGLE_START,			// ½Ì±Û ½ÃÀÛ
-		MULTI_HOST_START,		// È£½ºÆ®(ÁÖÃ¼) ½ÃÀÛ
-		MULTI_GUEST_START,		// °Ô½ºÆ®(ÀÇÁ¸) ½ÃÀÛ
+		PLAYTYPE,		// ï¿½ï¿½Æ¼ or ï¿½Ì±ï¿½ ï¿½ï¿½ï¿½ï¿½
+		WAIT,			// ï¿½ï¿½Æ¼ ï¿½ï¿½ï¿½
+		SINGLE_START,			// ï¿½Ì±ï¿½ ï¿½ï¿½ï¿½ï¿½
+		MULTI_HOST_START,		// È£ï¿½ï¿½Æ®(ï¿½ï¿½Ã¼) ï¿½ï¿½ï¿½ï¿½
+		MULTI_GUEST_START,		// ï¿½Ô½ï¿½Æ®(ï¿½ï¿½ï¿½ï¿½) ï¿½ï¿½ï¿½ï¿½
 		
 		SPAWN,
 		MOVE,
@@ -41,6 +41,39 @@ public:
 		FIRE,
 		LEAVE,
 		EXIT,
+		STC_IDCREATE,
+		CTS_IDCREATE,
+
+		STC_SPAWN,
+		CTS_SPAWN,
+
+		STC_MOVE,
+		CTS_MOVE,
+
+		STC_JUMP,
+		CTS_JUMP,
+
+		STC_DODGE,
+		CTS_DODGE,
+
+		STC_FIRE,
+		CTS_FIRE,
+
+		STC_OUT,
+		CTS_OUT,
+
+		STC_EXIT,
+		CTS_EXIT,
+
+		Test,
+
+		//JUNYOUNG_PROTOCOL
+		STC_NPC_SPAWN,
+		CTS_NPC_SPAWN,
+		STC_NPC_TRANSFORM,
+		CTS_NPC_TRANSFORM,
+		STC_ITEM_SPAWN,
+		CTS_ITEM_SPAWN,
 	};
 
 	void Function(Session* _session);
@@ -53,6 +86,9 @@ public:
 	void JumpProcess(Session* _session);
 	void DodgeProcess(Session* _session);
 	void FireProcess(Session* _session);
+	void NpcSpawnProcess(Session* _session);
+	void NpcTransformProcess(Session* _session);
+	void ItemSpawnProcess(Session* _session);
 
 	void ExitProcess(Session* _session);
 	void ForceExitProcess(Session* _session);
@@ -65,19 +101,24 @@ public:
 	int JumpDataMake(BYTE* _data, int _id);
 	int DodgeDataMake(BYTE* _data, int _id);
 	int FireDataMake(BYTE* _data, FireData _fireData);
-
+	int NpcSpawnDataMake(BYTE* _data, SpawnData_NPC _SpawnData_npc);
+	int NpcTransformDataMake(BYTE* _data, TransformData_NPC _TransformData_npc);
+	int ItemSpawnDataMake(BYTE* _data, SpawnData_Item _SpawnData_Item);
 
 	int ExitDataMake(BYTE* _data, int _id);
 	// unpacking
 	void MoveDataSplit(BYTE* _data, MoveData& _moveData);
 	void FireDataSplit(BYTE* _data, FireData& _fireData);
+	void NpcSpawnDataSplit(BYTE* _data, SpawnData_NPC& _SpawnData_npc);
+	void NpcTransformDataSplit(BYTE* _data, TransformData_NPC& _TransformData_npc);
+	void ItemSpawnDataSplit(BYTE* _data, SpawnData_Item& _SpawnData_Item);
 #pragma endregion
 private:
 	CriticalKey m_criticalKey;
 	int m_giveIdCounter;
 	list<Session*> m_playerList;
-	// ÇÃ·¹ÀÌ¾î Á¤º¸ - ¼¼¼Ç
-	// ¸ó½ºÅÍ Á¤º¸ - ¹«°ü
-	// ÁöÇü Á¤º¸ - ¹«°ü
+	// ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ ï¿½ï¿½ï¿½ï¿½ - ï¿½ï¿½ï¿½ï¿½
+	// ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ - ï¿½ï¿½ï¿½ï¿½
+	// ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ - ï¿½ï¿½ï¿½ï¿½
 };
 
